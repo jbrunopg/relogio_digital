826eee0e8f745e2408a32a3296b345d7908858f5
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-01 23:16:52 -0300

    Update relogio_digital.py

--- relogio_digital.py
+++ relogio_digital.py
@@ -3,7 +3,7 @@ import tkinter
 from datetime import datetime
 
 import pyglet
-pyglet.font.add_file('C:\Users\Magna Rios\Desktop\relogio digital/digital-7.ttf')
+pyglet.font.add_file("C:\Users\Magna Rios\Desktop\relogio digital/digital-7.ttf")
 
 ### cor ###
 cor1 = "#3d3d3d"  # preta
@@ -17,8 +17,8 @@ fundo = cor1
 cor = cor2
 
 janela =Tk()
-janela.title('')
-janela.geometry('440x180')
+janela.title("")
+janela.geometry("440x180")
 janela.resizable(width=FALSE, height=FALSE)
 janela.configure(bg=cor1)
 
@@ -26,23 +26,22 @@ def relogio():
 
 tempo = datetime.now()
 
-    hora = tempo.strftime('%H:%M:%S')
+    hora = tempo.strftime("%H:%M:%S")
     dia_semana = tempo.strftime(%A)
     dia = tempo.day
-    mes = tempo.strftime('%b')
-    ano = tempo.strftime('%Y')
+    mes = tempo.strftime("%b")
+    ano = tempo.strftime("%Y")
 
     l1.config(text=hora)
     l1.after(200, relogio)
-    l2.config(text=dia_semana +'  '+ str(dia) + 
-                '/' + str(mes) + '/' + str(ano))
+    l2.config(text=dia_semana +"  "+ str(dia) + "/" + str(mes) + "/" + str(ano))
 
     print(mes)
-
-l1=Label(janela, text='', font=('digital-7 100'), bg=fundo, fg=cor)
+    
+l1=Label(janela, text="", font=("digital-7 100"), bg=fundo, fg=cor)
 l1.grid(row=0, column=0, sticky=NW, padx=5)
 
-l2=Label(janela, text='', font=('digital-7 100'), bg=fundo, fg=cor)
+l2=Label(janela, text="", font=("digital-7 100"), bg=fundo, fg=cor)
 l2.grid(row=0, column=0, sticky=NW, padx=5)
 
 relogio()
